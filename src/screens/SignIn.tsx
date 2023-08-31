f0de5a5a0377d544d7bf538274ef2042779aa54c
Thiago Leite <leite.tjs@gmail.com>
2023-08-31 16:03:05 -0300

    feat: input component

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,39 @@
+import { VStack, Image, Center, Text, Heading } from 'native-base'
+
+import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
+import Input from '@components/Input/Input'
+
+export default function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700" px={10}>
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas Treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize={'xl'} mb={6} fontFamily={'heading'}>
+          Acesse sua conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
+      </Center>
+    </VStack>
+  )
+}
