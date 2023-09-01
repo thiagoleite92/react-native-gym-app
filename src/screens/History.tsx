590b79373832a5ff7a0567f9d3775d45771c0f68
Thiago Leite <leite.tjs@gmail.com>
2023-09-01 02:23:21 -0300

    feat: history component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,18 +1,54 @@
-import { Center, Text, VStack } from 'native-base'
+import { useState } from 'react'
+import { Center, Heading, Text, VStack, SectionList } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader/ScreenHeader'
 import { HistoryCard } from '@components/HistoryCard/HistoryCard'
 
-import React from 'react'
-
 export default function History() {
+  const [history, setHistory] = useState([
+    {
+      title: 'Hoje',
+      data: ['Pizza', 'Burger', 'Risotto'],
+    },
+    {
+      title: 'Ontem',
+      data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+    },
+    {
+      title: 'Anteontem',
+      data: ['Water', 'Coke', 'Beer'],
+    },
+    {
+      title: 'Retrasado',
+      data: ['Cheese Cake', 'Ice Cream'],
+    },
+  ])
+
   return (
-    <VStack>
+    <VStack flex={1}>
       <ScreenHeader title="Histórico" />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+
+      <SectionList
+        sections={history}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={5} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          history.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   )
 }
